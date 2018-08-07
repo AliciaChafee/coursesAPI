f92b7888b9d960bb2e2fc763aa032f29b4a54670
Alicia Chafee <alicia.chafee@maxxpotential.com>
2018-08-07 16:25:50 -0400

    working kinda

--- app.py
+++ app.py
@@ -1,37 +1,75 @@
 from flask import Flask, render_template, jsonify, json, request, url_for
-
+from flask_restful import Api, Resource, reqparse
+import database
 
 
 
 app = Flask(__name__)
+api = Api(app)
 
+
+class Books(Resource):
+	def get(self, name):
+		for book in books:
+			if(name == book['title']):
+				return book, 200
+		return "Book {} not found".format(name), 404
+
+	def post(self, name):
+		parser = reqparse.RequestParser()
+		parser.add_argument("author")
+		parser.add_argument("published")
+		args = parser.parse_args()
+		newBook = database.mainBook(name, args['author'], args['published'])
+		return newBook
+
+class Movies(Resource):
+	def get(self, name):
+		for movie in movies:
+			if(name == movie['title']):
+				return movie, 200
+		return "Movie {} not found".format(name), 404
+
+	def post(self, name):
+		parser = reqparse.RequestParser()
+		parser.add_argument("director")
+		parser.add_argument("released")
+		args = parser.parse_args()
+		newMovie = database.mainMovie(name, args['director'], args['released'])
+		return json.dumps(newMovie)
+
+
+class AllMovies(Resource):
+	def get(self):
+		movies = database.allMovies();
+		return movies, 200
+
+class AllBooks(Resource):
+	def get(self):
+		books = database.allBooks();
+		return books, 200
+
+
+api.add_resource(Books, "/book/<string:name>")
+api.add_resource(Movies, "/movie/<string:name>")
+api.add_resource(AllBooks, "/books")
+api.add_resource(AllMovies, "/movies")
+
+
+# Routes ***************
 
 @app.route('/home')
 @app.route('/')
 def home():
 	return render_template('home.html')
 
-@app.route('/movies')
-def movies():
+@app.route('/moviesPage')
+def moviesPage():
 	return render_template('movies.html')
 
-@app.route('/books')
-def books():
+@app.route('/booksPage')
+def booksPageboo():
 	return render_template('books.html')
-
-@app.route('/Allbooks')
-def allBooks():
-	cursor = conn.execute
-
-
-@app.route('/addBook', methods=['GET', 'POST'])
-def addBook():
-	title = request.args.get('title')
-	author = request.args.get('author')
-	year = request.args.get('year')
-
-	newBook = database.mainBook(title, author, year)
-	return newBook
 
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
+// Home Page ******************************
 
 	$('#newBookForm').hide();
 	$('#newMovieForm').hide();
 	$('#book-added-response').hide();
 	$('#movie-added-response').hide();
+
 
 	$('#addMovie').on('click', function() {
 		$('#newBookForm').hide();
@@ -16,19 +18,28 @@ $(document).ready(function() {
 		$('#newBookForm').show();
 	});
 
+
+// Books Page *************************
+
+	// Add new book to library database
+	$("#bookTableDiv").hide();
+
 	$('#submitBook').on('click', function() {
 		var bookTitle = $('#newBookTitle').val().toUpperCase();
 		var bookAuthor = $('#author').val().toUpperCase();
 		var bookYear = $('#yearPublished').val();
 
+		var url = "/book/" + bookTitle;
+
 		$.ajax({
-			data: { title : bookTitle, author: bookAuthor, year: bookYear },
-			type: 'GET',
-			url: 'addBook',
+			data: { title : bookTitle, author: bookAuthor, published: bookYear },
+			type: 'POST',
+			url: url,
 			dataType: 'json',
 			success: function(response){
+				console.log("response: " + response);
 				console.log("Success adding book");
-				$('#book-added-response').show();
+				//$('#book-added-response').show();
 
 
 			},
@@ -37,6 +48,79 @@ $(document).ready(function() {
 			}
 		});
 		
+	});
+
+	// View all books in library database
+
+	$("#viewBooks").on('click', function() {
+
+		$("#books-body").empty();
+		$("#bookTableDiv").toggle();
+		
+			$.ajax({
+				type: 'GET',
+				url: 'books',
+				dataType: 'json',
+				success: function(response){
+					console.log(response);
+					for(var i = 0; i < response.data.length; i++) {
+						console.log(response.data[i]);
+					}
+					$("#books-body").append(response);
+				}
+			});
+	});
+
+
+// Movies Page ***************
+
+	// Add new movie to library database
+
+	$('#submitMovie').on('click', function() {
+		var movieTitle = $('#newMovieTitle').val().toUpperCase();
+		var movieDirector = $('#director').val().toUpperCase();
+		var movieYear = $('#yearReleased').val();
+
+		console.log(movieTitle);
+		console.log(movieDirector);
+		console.log(movieYear);
+
+		var url = "/movie/" + movieTitle;
+
+		console.log(url);
+
+		$.ajax({
+			data: { title : movieTitle, director: movieDirector, released: movieYear },
+			type: 'POST',
+			url: url,
+			dataType: 'json',
+			success: function(response){
+				console.log("response" + response);
+				console.log("Success adding movie");
+				$('#movie-added-response').show();
+
+
+			},
+			error: function(error) {
+				console.log("Error adding movie: " + error);
+			}
+		});
+		
+	});
+
+	// View all movies in library database
+
+	$("#viewMovies").on('click', function() {
+		$("#movies-body").empty();
+		
+			$.ajax({
+				type: 'GET',
+				url: 'movies',
+				success: function(response){
+					console.log(response);
+					$("#movies-body").append(response);
+				}
+			});
 	});
 
 
